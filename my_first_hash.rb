bd0024132cf8b69b08d5f97689ea03c06c7c8ee7
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-27 17:47:35 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -31,7 +31,7 @@ def adding
   shipping_manifest["muskets"] = 2
 
   # add 4 gun powder to the shipping_manifest hash below
-
+  shipping_manifest["gun powder"]
 
 
   # return the shipping_manifest hash below
